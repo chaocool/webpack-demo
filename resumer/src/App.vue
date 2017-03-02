227ffcefca6e37286cba362b1d19e14886167de3
jack <cjichao@163.com>
2017-03-02 17:39:27 +0800

    完善登陆框

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -11,13 +11,14 @@
 </template>
 
 <script>
-  import 'normalize.css/normalize.css';
-  import './assets/reset.css';
 
-  import Topbar from './components/Topbar';
-  import ResumeEditor from './components/ResumeEditor';
-  import ResumePreview from './components/ResumePreview';
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
 
+import Topbar from './components/Topbar';
+import ResumeEditor from './components/ResumeEditor';
+import ResumePreview from './components/ResumePreview';
+import icons from './assets/icons'
 
 
   export default {
@@ -27,32 +28,35 @@
           text: '你好'
         }
     },
-    components: {Topbar, ResumeEditor, ResumePreview}
+    components: {Topbar, ResumeEditor, ResumePreview},
+    created(){
+        document.body.insertAdjacentHTML('afterbegin', icons)
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
   .page{
     height: 100vh;
     display: flex;
     flex-direction: column;  /*方向从左到右排列*/
     background: #EAEBEC;
-  }
-  .page>main{
+    >main{
     flex-grow: 1; /*定义项目的放大比例,如果为1 等分剩余空间*/
   }
-  .page>main {
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    /*justify-content: space-around; !*定义项目在主轴上的 对齐方式  space-around: 每个项目两侧的间隔相等。
+    >main {
+     min-width: 1024px;
+     max-width: 1440px;
+     margin-top: 16px;
+     margin-bottom: 16px;
+     display: flex;
+     /*justify-content: space-around; !*定义项目在主轴上的 对齐方式  space-around: 每个项目两侧的间隔相等。
                                       所以，项目之间的间隔比项目与边框的间隔大一倍。*/
-    justify-content: space-between;
-    padding: 0 16px;
-    width: 100%;
-    align-self: center;
+     justify-content: space-between;
+     padding: 0 16px;
+     width: 100%;
+     align-self: center;
+   }
   }
 
   #resumeEditor{
@@ -64,6 +68,13 @@
     flex-grow: 1;
     margin-left: 16px;
     background: #777;
+  }
+  svg.icon{
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+    vertical-align: -0.1em;
+    font-size: 16px;
   }
 
 </style>
--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -1,18 +1,133 @@
 <template>
   <div id="resumeEditor">
-    I am resumeEditor
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.config" :class="{active: item.field === selected}"
+          @click="selected = item.field">
+          <svg class="icon">
+            <use :xlink:href="`#icon-${item.icon}`"></use>
+          </svg>
+        </li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li v-for="item in resume.config" v-show="item.field === selected">
+        <div class="resumeField" v-for="(value, key) in resume[item.field]">
+          <label>{{key}}</label>
+          <input type="text" v-model="resume[item.field][key]">
+        </div>
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
     export default{
-      name: 'ResumeEditor'
+      name: 'ResumeEditor',
+      data(){
+          return{
+            selected: 'profile',
+            resume:{
+                visibleItems: ['profile', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
+              config: [
+                { field: 'profile', icon: 'id' },
+                { field: 'work history', icon: 'work' },
+                { field: 'education', icon: 'book' },
+                { field: 'projects', icon: 'heart' },
+                { field: 'awards', icon: 'cup' },
+                { field: 'contacts', icon: 'phone' },
+              ],
+              profile: {
+                    name: '',
+                    city: '',
+                    title: ''
+                },
+                'work history': [
+                  {company: 'AL', content:'我的第二份工作是'},
+                  {company: 'TX', content: '我的第一份工作是'},
+                ],
+                education: [],
+                projects: [],
+                awards: [],
+                contacts: [],
+//                others: []
+            }
+
+
+          }
+      }
     }
 </script>
 
-<style>
-  #resumeEditor{
+<style lang="scss" scoped>
+  #resumeEditor {
     background: #fff;
-    box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+    display: flex;
+    flex-direction: row;
+    overflow: auto;
+
+  >
+  nav {
+    width: 80px;
+    background: black;
+    color: white;
+
+  >
+  ol {
+
+  >
+  li {
+    height: 48px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 16px;
+    margin-bottom: 16px;
+
+  &
+  .active {
+    background: white;
+    color: black;
   }
+
+  }
+  }
+  }
+  >
+  .panels {
+    flex-grow: 1;
+
+  >
+  li {
+    padding: 24px;
+  }
+
+  }
+  svg.icon {
+    width: 24px;
+    height: 24px;
+  }
+
+  ol {
+    list-style: none;
+  }
+
+  .resumeField {
+
+  >
+  label {
+    display: block;
+  }
+
+  input[type=text] {
+    margin: 16px 0;
+    border: 1px solid #ddd;
+    box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+    width: 100%;
+    height: 40px;
+    padding: 0 8px;
+    }
+ }
+}
 </style>
